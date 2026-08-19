1f74a43e891d8464be9952da7afd920548932f67
Robert MacWha <github@macwha.com>
2026-08-19 15:07:11 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Faucet / Docs: https://faucet.privacy.ethrex.xyz/
+Explorer: https://dora.privacy.ethrex.xyz/
+RPC: https://rpc1.privacy.ethrex.xyz
---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "A basic flake with a shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.systems.url = "github:nix-systems/default";
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
+    inputs.systems.follows = "systems";
+  };
+  inputs.rust-overlay = {
+    url = "github:oxalica/rust-overlay";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay.overlays.default ];
+        };
+
+        rustToolchain = pkgs.rust-bin.stable."1.93.0".default.override {
+          extensions = [
+            "rust-src"
+            "llvm-tools"
+            "rust-analyzer"
+          ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            rustToolchain
+            pkgs.foundry
+          ];
+        };
+      }
+    );
+}
